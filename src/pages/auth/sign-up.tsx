4d1640de950f479fa91613451b4f98cd51b7e7b9
Alysson Bormannn <alysson.bormannn@gmail.com>
2024-05-09 10:51:48 -0400

    created page sign-in and sign-up

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,36 +1,26 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z.string().email(),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignUpForm = z.infer<typeof signUpForm>
 
-export function SignIn() {
+export function SignUp() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignUpForm>()
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     await new Promise((resolve) => setTimeout(resolve, 2000))
-
-    toast.success('Enviamos um link de autenticacao para seu e-mail.', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => {
-          handleSignIn(data)
-        },
-      },
-    })
   }
 
   return (
@@ -40,21 +30,21 @@ export function SignIn() {
         <div className="flex w-[350] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tighter">
-              Acessar Painel
+              Criar conta gratis
             </h1>
             <p className="text-sm text-muted-foreground">
-              Acompanhe suas venda pelo painel do parceiro!
+              Seja um parceiro e comece suas vendas!
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="sapce-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
 
             <Button className="w-full" type="submit" disabled={isSubmitting}>
-              Acessar painel
+              Finalizar Cadastro
             </Button>
           </form>
         </div>
